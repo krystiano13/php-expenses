fded24b7401c4d16c350b97902d7873483e6d68f
krystiano13 <crystianotv@gmail.com>
2023-11-23 12:26:47 +0100

    implement rendering table tag

--- src/Table.php
+++ src/Table.php
@@ -11,18 +11,24 @@ class Table
     {
         $data = new Data('./src/sample_1.csv');
         $this -> expensesTable = $data -> getTable();
-        $this -> createHeadRow();
+        $this -> createTable();
+    }
+
+    public function createTable() {
+        echo '<table>';
+            $this -> createHeadRow();
+        echo '</table>';
     }
 
     private function createHeadRow(){
         $headRow = (array)$this -> expensesTable[0];
 
-        echo '<div>';
+        echo '<tr>';
 
         for($i = 0; $i < count($headRow); $i++) {
-            echo "<p>$headRow[$i]</p>";
+            echo "<th>$headRow[$i]</th>";
         }
 
-        echo '</div>';
+        echo '</tr>';
     }
 }
