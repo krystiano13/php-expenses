7901e128d1fc905f223f6d24a0e6edb31e1a817c
krystiano13 <crystianotv@gmail.com>
2023-11-23 12:14:11 +0100

    create website boilerplate

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 
 include 'vendor/autoload.php';
 
-use App\Data;
+?>
 
-$data = new Data('./src/sample_1.csv');
-
-var_dump($data -> getTable()[0]);
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="author" content="Krystian Zieja" />
+        <meta name="keywords" content="expenses, tracking, tracker, expense" />
+        <meta name="description" content="PHP expenses tracker with csv file import" />
+        <title>PHP expenses</title>
+    </head>
+    <body></body>
+</html>
